803d9a15622924536aab4c8b8d0fcb1805ddf423
Jolan <jolan.robert56@gmail.com>
2023-03-10 14:51:32 +0100

    Merge Fix

--- Assets/Scripts/Player/PlayerSwitchColor.cs
+++ Assets/Scripts/Player/PlayerSwitchColor.cs
@@ -33,12 +33,6 @@ namespace Player
             color = newColor;
             sphereRenderer.material.color = color == PlayerColor.Blue ? Color.blue : Color.red;
         }
-        
-        private void Start()
-        {
-            if (PlayerInputManager.instance)
-                color = (PlayerColor)PlayerInputManager.instance.playerCount - 1;
-        }
 
         public void Switch()
         {
@@ -59,8 +53,6 @@ namespace Player
 
     public enum PlayerColor
     {
-        Blue,
-        Red,
-        None
+        Blue, Red, None
     }
 }
