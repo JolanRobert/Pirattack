f04f5730fce38edd96a43a099ea48e1c736c87a6
Jolan <jolan.robert56@gmail.com>
2023-03-10 15:57:09 +0100

    Task Bed Final

--- Assets/Scripts/RespawnTrigger.cs
+++ Assets/Scripts/RespawnTrigger.cs
@@ -3,40 +3,37 @@ using MyBox;
 using Player;
 using UnityEngine;
 
-namespace InteractiveTrigger
+public class RespawnTrigger : InteractiveElement
 {
-    public class RespawnTrigger : InteractiveElement
+    [Separator("Respawn Trigger")]
+    [SerializeField] private PlayerRespawn playerRespawn;
+
+    public void HandleInput(bool respawnInput)
     {
-        [Separator("Respawn Trigger")]
-        [SerializeField] private PlayerRespawn playerRespawn;
-
-        public void HandleInput(bool respawnInput)
-        {
-            if (respawnInput) IncreaseBar();
-            else DecreaseBar();
-        }
+        if (respawnInput) IncreaseBar();
+        else DecreaseBar();
+    }
         
-        private void IncreaseBar()
-        {
-            float newAmount = progressBar.fillAmount + Time.deltaTime / playerRespawn.RespawnDuration;
+    private void IncreaseBar()
+    {
+        float newAmount = progressBar.fillAmount + Time.deltaTime / playerRespawn.RespawnDuration;
 
-            progressBar.DOKill();
-            Tween tween = progressBar.DOFillAmount(newAmount, Time.deltaTime).SetEase(Ease.Linear);
-            if (newAmount >= 1) tween.onComplete += Complete;
-        }
+        progressBar.DOKill();
+        Tween tween = progressBar.DOFillAmount(newAmount, Time.deltaTime).SetEase(Ease.Linear);
+        if (newAmount >= 1) tween.onComplete += Complete;
+    }
 
-        private void DecreaseBar()
-        {
-            if (progressBar.fillAmount == 0) return;
-            float newAmount = progressBar.fillAmount - Time.deltaTime / playerRespawn.RespawnDuration;
+    private void DecreaseBar()
+    {
+        if (progressBar.fillAmount == 0) return;
+        float newAmount = progressBar.fillAmount - Time.deltaTime / playerRespawn.RespawnDuration;
 
-            progressBar.DOKill();
-            progressBar.DOFillAmount(newAmount, Time.deltaTime).SetEase(Ease.Linear);
-        }
+        progressBar.DOKill();
+        progressBar.DOFillAmount(newAmount, Time.deltaTime).SetEase(Ease.Linear);
+    }
 
-        private void Complete()
-        {
-            playerRespawn.Respawn();
-        }
+    private void Complete()
+    {
+        playerRespawn.Respawn();
     }
 }
